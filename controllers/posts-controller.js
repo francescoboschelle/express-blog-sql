3af88a439843b0825c54870a42ebef1446d32c57
francescoboschelle <francibossch@gmail.com>
2025-07-10 16:48:08 +0200

    Implement destroy function in posts controller with error handling for deleting a post by ID

--- controllers/posts-controller.js
+++ controllers/posts-controller.js
@@ -63,7 +63,35 @@ function update(req, res) {}
 function modify(req, res) {}
 
 // DELETE /:id
-function destroy(req, res) {}
+function destroy(req, res) {
+  const id = parseInt(req.params.id);
+
+  if (isNaN(id)) {
+    return res.status(400).json({
+      error: true,
+      message: "Bad Request",
+    });
+  }
+
+  const sql = "DELETE FROM posts WHERE id = ?;";
+
+  connection.query(sql, [id], (err, results) => {
+    if (err)
+      return res.status(500).json({
+        error: true,
+        message: "Internal Server Error",
+      });
+
+    if (results.affectedRows === 0) {
+      return res.status(404).json({
+        error: true,
+        message: "Not Found",
+      });
+    }
+
+    return res.status(204).send();
+  });
+}
 
 export default {
   index,
